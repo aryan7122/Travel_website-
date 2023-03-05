f5392c773d4e74c79b6b045517597bddf837c4d5
Aryan <aryankushwahalifenote@gmail.com>
2023-03-05 10:30:28 +0530

    V1.0.0.0

---
+++ App.js
@@ -0,0 +1,9 @@
+Array.from(document.getElementsByTagName('input')).forEach((e, i)=> {
+    e.addEventListener('keyup', (el) => {
+        if (e.value.length > 0) {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
+        } else {
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
+        }
+    })
+})
